a188d3f38a7ec726b22ad0e1f6fdd8e458c9e2b6
Derrek Meno <DERREKWHO15@gmail.com>
2020-03-11 20:57:02 -0700

    thank you boyer

---
+++ script.js
@@ -0,0 +1,27 @@
+// 1. Write a console.log() statement that answers the following question: What worked
+// well for you over the last 9 weeks that aided in your learning process?
+
+console.log(
+  "What worked well for you over the last 9 weeks that aided in your learning process?"
+);
+console.log("the demos really helped to give me an understanding of what was going on. I am not a person who really enjoys lectures and I am very much a visual and hands on learner so the demos then assignments helped a lot with em understanding the concepts");
+
+// 2. Write a console.log() statement that answers the following question: What would you
+// change about the course?
+
+console.log("What would you change about the course?");
+console.log("I cannot think of anything to change. I had some trouble throughout but that is due to my lack of knowledge of HTML and other basics needed to understand JavaScript.");
+
+// 3. Write a console.log() statement that answers the following question: Did you learn
+// what you expected to learn in this course?
+
+console.log("Did you learn what you expected to learn in this course?");
+console.log("yes");
+
+// 4. Write a console.log() statement that answers the following question: What concepts
+// were new to you and what concepts had you encountered already in the past?
+
+console.log(
+  "What concepts were new to you and what concepts had you encountered already in the past?"
+);
+console.log("they were almost all new. the only concept i recognized was the while loops");
